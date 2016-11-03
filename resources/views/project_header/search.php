58a7bd9866399ada8df67355b786da262af4d7d1
phecho <phecho@phecho.com>
2016-11-03 08:56:06 +0800

    Change categories icon

--- resources/views/project_header/search.php
+++ resources/views/project_header/search.php
@@ -40,7 +40,7 @@
             <?php if (isset($categories_list) && ! empty($categories_list)): ?>
             <div class="input-addon-item">
                 <div class="dropdown">
-                    <a href="#" class="dropdown-menu dropdown-menu-link-icon" title="<?= t('Category filters') ?>"><i class="fa fa-folder-o"></i> <i class="fa fa-caret-down"></i></a>
+                    <a href="#" class="dropdown-menu dropdown-menu-link-icon" title="<?= t('Category filters') ?>"><i class="fa fa-sitemap"></i> <i class="fa fa-caret-down"></i></a>
                     <ul>
                         <li><a href="#" class="filter-helper" data-unique-filter="category:none"><?= t('No category') ?></a></li>
                         <?php foreach ($categories_list as $category): ?>
