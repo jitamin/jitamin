060e0681f05c8d19d58a0f91a79f2161bddc28ad
phecho <phecho@phecho.com>
2016-11-24 15:38:27 +0800

    Adjust the position of project information

--- resources/views/project_view/show.php
+++ resources/views/project_view/show.php
@@ -2,7 +2,7 @@
     <?= $this->projectHeader->render($project, 'ProjectViewController', 'show') ?>
     <?= $this->render('project_view/columns', array('project' => $project)) ?>
     <?= $this->render('project_view/description', array('project' => $project)) ?>
-    <?= $this->render('project_view/attachments', array('project' => $project, 'images' => $images, 'files' => $files)) ?>
     <?= $this->render('project_view/information', array('project' => $project, 'users' => $users, 'roles' => $roles)) ?>
+    <?= $this->render('project_view/attachments', array('project' => $project, 'images' => $images, 'files' => $files)) ?>
     <?= $this->render('project_view/activity', array('project' => $project, 'events' => $events)) ?>
 </section>
