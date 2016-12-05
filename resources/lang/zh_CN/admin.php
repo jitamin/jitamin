d3da0307a4ab99b710a7a50210480e08bbc59695
phecho <phecho@163.com>
2016-12-05 13:49:35 +0800

    Update language

---
+++ resources/lang/zh_CN/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Projects management' => '项目管理',
+    'Users management' => '用户管理',
+    'Groups management' => '用户组管理',
+    'Plugins management' => '插件管理',
+    'Manage projects' => '项目管理',
+    'Settings' => '设置',
+    'Application settings' => '系统设置',
+];
