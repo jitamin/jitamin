120cc57da642c12a05f34f02908a6803c2ceddbb
phecho <phecho@163.com>
2016-12-09 11:47:43 +0800

    Improve footer

--- resources/views/_partials/footer.php
+++ resources/views/_partials/footer.php
@@ -1,3 +1,6 @@
-<footer class="container">
-    <p>Copyright &copy; 2016 <strong><a href="http://hiject.com">Hiject</a></strong> Version <?= APP_VERSION ?>  All rights reserved.</p>
+<footer>
+    <div class="container">
+        <hr />
+        <p>Copyright &copy; 2016 <strong><a href="http://hiject.com">Hiject</a></strong> Version <?= APP_VERSION ?>  All rights reserved.</p>
+    </div>
 </footer>
