8a2bdd690c0c4d8a82da69f0e371b52dbc61af65
Guan Shiliang <phecho@163.com>
2016-12-12 10:42:13 +0000

    Apply fixes from StyleCI

--- resources/views/dashboard/stars.php
+++ resources/views/dashboard/stars.php
@@ -1,1 +1,1 @@
-<?= $this->render('dashboard/_partials/projects', ['paginator' => $paginator, 'user' => $user]) ?>
+<?= $this->render('dashboard/_partials/projects', ['paginator' => $paginator, 'user' => $user]);
