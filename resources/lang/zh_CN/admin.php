192da698f6645cabb6ef7618a99599b79907bdfd
phecho <phecho@163.com>
2016-12-13 16:38:16 +0800

    Tweak languages

--- resources/lang/zh_CN/admin.php
+++ resources/lang/zh_CN/admin.php
@@ -3,7 +3,7 @@
 return [
     'Projects management' => '项目管理',
     'Users management' => '用户管理',
-    'Groups management' => '用户组管理',
+    'Groups management' => '团队管理',
     'Plugins management' => '插件管理',
     'Manage projects' => '项目管理',
     'Settings' => '设置',
