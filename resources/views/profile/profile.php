8a494f9df5609d82640ed045345d2da613fbfec5
phecho <phecho@163.com>
2016-12-14 08:57:06 +0800

    Add a new avatar css

--- resources/views/profile/profile.php
+++ resources/views/profile/profile.php
@@ -2,7 +2,7 @@
     <div class="page-header">
         <h2><?= e($user['name'] ?: $user['username']) ?></h2>
     </div>
-    <?= $this->avatar->render($user['id'], $user['username'], $user['name'], $user['email'], $user['avatar_path']) ?>
+    <?= $this->avatar->render($user['id'], $user['username'], $user['name'], $user['email'], $user['avatar_path'], 'avatar-left', 84) ?>
     <ul class="listing">
         <li><?= t('Username:') ?> <strong><?= $this->text->e($user['username']) ?></strong></li>
         <li><?= t('Name:') ?> <strong><?= $this->text->e($user['name']) ?: t('None') ?></strong></li>
