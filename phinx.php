d8e1de30813814d3ee315d4eafd550479d78ac1b
Guan Shiliang <phecho@163.com>
2016-12-22 21:40:05 +0800

    Apply fixes from StyleCI (#42)

--- phinx.php
+++ phinx.php
@@ -1,18 +1,28 @@
 <?php
-    return array(
-        "paths" => array(
-            "migrations" => "database/migrations"
-        ),
-        "environments" => array(
-            "default_migration_table" => "migrations",
-            "default_database" => "dev",
-            "dev" => array(
-                "adapter" => $_ENV['DB_DRIVER'],
-                "host" => $_ENV['DB_HOST'],
-                "name" => $_ENV['DB_NAME'],
-                "user" => $_ENV['DB_USER'],
-                "pass" => $_ENV['DB_PASS'],
-                "port" => $_ENV['DB_PORT']
-            )
-        )
-    );
+
+/*
+ * This file is part of Jitamin.
+ *
+ * Copyright (C) 2016 Jitamin Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+    return [
+        'paths' => [
+            'migrations' => 'database/migrations',
+        ],
+        'environments' => [
+            'default_migration_table' => 'migrations',
+            'default_database'        => 'dev',
+            'dev'                     => [
+                'adapter' => $_ENV['DB_DRIVER'],
+                'host'    => $_ENV['DB_HOST'],
+                'name'    => $_ENV['DB_NAME'],
+                'user'    => $_ENV['DB_USER'],
+                'pass'    => $_ENV['DB_PASS'],
+                'port'    => $_ENV['DB_PORT'],
+            ],
+        ],
+    ];
