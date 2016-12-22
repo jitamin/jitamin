69393dbbb18eaac83cd29496e30265523cbdec41
Guan Shiliang <phecho@163.com>
2016-12-23 00:25:40 +0800

    Apply fixes from StyleCI (#43)

--- database/seeds/LinkSeeder.php
+++ database/seeds/LinkSeeder.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Jitamin.
+ *
+ * Copyright (C) 2016 Jitamin Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Phinx\Seed\AbstractSeed;
 
 class LinkSeeder extends AbstractSeed
@@ -11,47 +20,47 @@ class LinkSeeder extends AbstractSeed
     {
         $data = [
           [
-              'label'    => 'relates to',
+              'label'          => 'relates to',
               'opposite_id'    => 0,
           ],
           [
-              'label'    => 'blocks',
+              'label'          => 'blocks',
               'opposite_id'    => 3,
           ],
           [
-              'label'    => 'is blocked by',
+              'label'          => 'is blocked by',
               'opposite_id'    => 2,
           ],
           [
-              'label'    => 'duplicates',
+              'label'          => 'duplicates',
               'opposite_id'    => 5,
           ],
           [
-              'label'    => 'is duplicated by',
+              'label'          => 'is duplicated by',
               'opposite_id'    => 4,
           ],
           [
-              'label'    => 'is a child of',
+              'label'          => 'is a child of',
               'opposite_id'    => 7,
           ],
           [
-              'label'    => 'is a parent of',
+              'label'          => 'is a parent of',
               'opposite_id'    => 6,
           ],
           [
-              'label'    => 'targets milestone',
+              'label'          => 'targets milestone',
               'opposite_id'    => 9,
           ],
           [
-              'label'    => 'is a milestone of',
+              'label'          => 'is a milestone of',
               'opposite_id'    => 8,
           ],
           [
-              'label'    => 'fixes',
+              'label'          => 'fixes',
               'opposite_id'    => 11,
           ],
           [
-              'label'    => 'is fixed by',
+              'label'          => 'is fixed by',
               'opposite_id'    => 10,
           ],
         ];
