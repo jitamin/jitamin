5c553eff09c000add4a4892dd71e851553e5cec3
phecho <phecho@163.com>
2016-12-23 00:25:15 +0800

    Add link seeder

---
+++ database/seeds/LinkSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class LinkSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     */
+    public function run()
+    {
+        $data = [
+          [
+              'label'    => 'relates to',
+              'opposite_id'    => 0,
+          ],
+          [
+              'label'    => 'blocks',
+              'opposite_id'    => 3,
+          ],
+          [
+              'label'    => 'is blocked by',
+              'opposite_id'    => 2,
+          ],
+          [
+              'label'    => 'duplicates',
+              'opposite_id'    => 5,
+          ],
+          [
+              'label'    => 'is duplicated by',
+              'opposite_id'    => 4,
+          ],
+          [
+              'label'    => 'is a child of',
+              'opposite_id'    => 7,
+          ],
+          [
+              'label'    => 'is a parent of',
+              'opposite_id'    => 6,
+          ],
+          [
+              'label'    => 'targets milestone',
+              'opposite_id'    => 9,
+          ],
+          [
+              'label'    => 'is a milestone of',
+              'opposite_id'    => 8,
+          ],
+          [
+              'label'    => 'fixes',
+              'opposite_id'    => 11,
+          ],
+          [
+              'label'    => 'is fixed by',
+              'opposite_id'    => 10,
+          ],
+        ];
+
+        $links = $this->table('links');
+        $links->insert($data)
+              ->save();
+    }
+}
