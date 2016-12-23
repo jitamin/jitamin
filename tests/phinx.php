fa8d5e9e25c433235848111ca4da1dfc8b879769
Guan Shiliang <phecho@163.com>
2016-12-23 02:36:53 +0000

    Apply fixes from StyleCI

--- tests/phinx.php
+++ tests/phinx.php
@@ -1,34 +1,44 @@
 <?php
-    return array(
-        "paths" => array(
-            "migrations" => "database/migrations",
-            "seeds" => "database/seeds",
-        ),
-        "environments" => array(
-            "default_migration_table" => "migrations",
-            "default_database" => "mysql",
-            "mysql" => array(
-                "adapter" => "mysql",
-                "host" => '127.0.0.1',
-                "name" => 'jitamin',
-                "user" => 'root',
-                "pass" => '',
-                "port" => '3306',
-                "charset" => 'utf8',
-            ),
-            "postgres" => array(
-                "adapter" => "pgsql",
-                "host" => '127.0.0.1',
-                "name" => 'jitamin',
-                "user" => 'postgres',
-                "pass" => '',
-                "port" => '5432',
-                "charset" => 'utf8',
-            ),
-            "sqlite" => array(
-                "adapter" => "sqlite",
-                "name" => 'jitamin',
-                "memory" => true
-            )
-        )
-    );
+
+/*
+ * This file is part of Jitamin.
+ *
+ * Copyright (C) 2016 Jitamin Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+    return [
+        'paths' => [
+            'migrations' => 'database/migrations',
+            'seeds'      => 'database/seeds',
+        ],
+        'environments' => [
+            'default_migration_table' => 'migrations',
+            'default_database'        => 'mysql',
+            'mysql'                   => [
+                'adapter' => 'mysql',
+                'host'    => '127.0.0.1',
+                'name'    => 'jitamin',
+                'user'    => 'root',
+                'pass'    => '',
+                'port'    => '3306',
+                'charset' => 'utf8',
+            ],
+            'postgres' => [
+                'adapter' => 'pgsql',
+                'host'    => '127.0.0.1',
+                'name'    => 'jitamin',
+                'user'    => 'postgres',
+                'pass'    => '',
+                'port'    => '5432',
+                'charset' => 'utf8',
+            ],
+            'sqlite' => [
+                'adapter' => 'sqlite',
+                'name'    => 'jitamin',
+                'memory'  => true,
+            ],
+        ],
+    ];
