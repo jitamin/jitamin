61948b56a6978fe9d3ed4e2ec73ff11433a26d74
phecho <phecho@163.com>
2016-12-26 14:09:45 +0800

    Clean up

--- resources/views/header/user_notifications.php
+++
@@ -1,7 +0,0 @@
-<li class="notification">
-<?php if ($this->user->hasNotifications()): ?>
-    <?= $this->url->link('<i class="fa fa-bell web-notification-icon"></i>', 'DashboardController', 'notifications', [], false, '', t('You have unread notifications')) ?>
-<?php else: ?>
-    <?= $this->url->link('<i class="fa fa-bell"></i>', 'DashboardController', 'notifications', [], false, '', t('You have no unread notifications')) ?>
-<?php endif ?>
-</li>
