38471d442fb1a3b6ae61321d7624aa268678cc0b
phecho <phecho@163.com>
2016-12-26 13:51:10 +0800

    Tweak dashboard stuff

--- resources/lang/zh_CN/jitamin.php
+++ resources/lang/zh_CN/jitamin.php
@@ -827,7 +827,6 @@ return [
     'Assignee changed on task #%d' => '任务 #%d 的指派人已变更',
     '%d overdue tasks' => '%d条超期任务',
     'Task #%d is overdue' => '任务#%d已超期',
-    'No new notifications.' => '没有新通知',
     'Mark all as read' => '标记所有为已读',
     'Mark as read' => '标为已读',
     'Total number of tasks in this column across all swimlanes' => '此任务栏下的任务数（跨泳道）',
