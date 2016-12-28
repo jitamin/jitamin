9be5bede89b1deb5376ffa63ab9a1fbcd93baf71
Guan Shiliang <phecho@163.com>
2016-12-28 11:30:36 +0800

    Apply fixes from StyleCI (#77)

--- database/migrations/20161228031419_alter_table_users_add_layout_column.php
+++ database/migrations/20161228031419_alter_table_users_add_layout_column.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Jitamin.
+ *
+ * Copyright (C) 2016 Jitamin Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Phinx\Migration\AbstractMigration;
 
 class AlterTableUsersAddLayoutColumn extends AbstractMigration
