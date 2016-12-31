231c0e6c063fcb8362872833439e57bb88854157
phecho <phecho@163.com>
2016-12-31 23:18:41 +0800

    Added github link on footer

--- resources/views/_partials/footer.php
+++ resources/views/_partials/footer.php
@@ -1,3 +1,5 @@
 <div class="footer">
-    <p>Copyright &copy; 2016 <strong><a href="http://jitamin.com">Jitamin</a></strong> Version <?= APP_VERSION ?>  All rights reserved.</p>
+    <p>Copyright &copy; 2016 <a href="http://jitamin.com">Jitamin</a> Version <?= APP_VERSION ?>  All rights reserved.
+    <span class="pull-right">Jitamin on <a href="https://github.com/jitamin/jitamin" title="Jitamin is developed on Github"><i class="fa fa-github"></i> Github</a></span>
+    </p>
 </div>
