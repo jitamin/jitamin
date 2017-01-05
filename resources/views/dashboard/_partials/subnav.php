1c641743c3fb6935832f7012fb884b4f900d8f6c
phecho <phecho@163.com>
2017-01-05 15:34:54 +0800

    Fix active item setting

--- resources/views/dashboard/_partials/subnav.php
+++ resources/views/dashboard/_partials/subnav.php
@@ -1,24 +1,24 @@
 <div class="page-header">
     <ul class="nav nav-tabs">
-        <li <?= $this->app->setActive('Dashboard/ProjectController', 'index') ?>>
+        <li <?= $this->app->setActive('Dashboard/ProjectController', 'index', 'dashboard') ?>>
             <?= $this->url->link(t('My projects'), 'Dashboard/ProjectController', 'index') ?>
         </li>
-        <li <?= $this->app->setActive('Dashboard/ProjectController', 'starred') ?>>
+        <li <?= $this->app->setActive('Dashboard/ProjectController', 'starred', 'dashboard') ?>>
             <?= $this->url->link(t('My stars'), 'Dashboard/ProjectController', 'starred') ?>
         </li>
-        <li <?= $this->app->setActive('Dashboard/DashboardController', 'tasks') ?>>
+        <li <?= $this->app->setActive('Dashboard/DashboardController', 'tasks', 'dashboard') ?>>
             <?= $this->url->link(t('My tasks'), 'Dashboard/DashboardController', 'tasks') ?>
         </li>
-        <li <?= $this->app->setActive('Dashboard/DashboardController', 'subtasks') ?>>
+        <li <?= $this->app->setActive('Dashboard/DashboardController', 'subtasks', 'dashboard') ?>>
             <?= $this->url->link(t('My subtasks'), 'Dashboard/DashboardController', 'subtasks') ?>
         </li>
-        <li <?= $this->app->setActive('Dashboard/DashboardController', 'calendar') ?>>
+        <li <?= $this->app->setActive('Dashboard/DashboardController', 'calendar', 'dashboard') ?>>
             <?= $this->url->link(t('My calendar'), 'Dashboard/DashboardController', 'calendar') ?>
         </li>
-        <li <?= $this->app->setActive('Dashboard/DashboardController', 'activities') ?>>
+        <li <?= $this->app->setActive('Dashboard/DashboardController', 'activities', 'dashboard') ?>>
             <?= $this->url->link(t('My activities'), 'Dashboard/DashboardController', 'activities') ?>
         </li>
-        <li <?= $this->app->setActive('Dashboard/DashboardController', 'notifications') ?>>
+        <li <?= $this->app->setActive('Dashboard/DashboardController', 'notifications', 'dashboard') ?>>
             <?= $this->url->link(t('My notifications'), 'Dashboard/DashboardController', 'notifications') ?>
         </li>
         <?= $this->hook->render('template:dashboard:subside', ['user' => $user]) ?>
