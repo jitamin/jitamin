5ec10de9a5c190bfbe0a552e666ad8e17a467a98
phecho <phecho@163.com>
2017-01-05 11:17:51 +0800

    Tweak admin subnav

--- resources/views/admin/_partials/subnav.php
+++ resources/views/admin/_partials/subnav.php
@@ -3,22 +3,22 @@
         <li <?= $this->app->setActive('Admin/AdminController', 'index') ?>>
             <?= $this->url->link(t('Overview'), 'Admin/AdminController', 'index') ?>
         </li>
-        <li <?= $this->app->setActive('Admin/SettingController', 'index') ?>>
+        <li <?= $this->app->setActive('Admin/SettingController') ?>>
             <?= $this->url->link(t('Application settings'), 'Admin/SettingController', 'index') ?>
         </li>
-        <li <?= $this->app->setActive('Admin/UserController', 'index') ?>>
+        <li <?= $this->app->setActive('Admin/UserController') ?>>
             <?= $this->url->link(t('Users management'), 'Admin/UserController', 'index') ?>
         </li>
-        <li <?= $this->app->setActive('Admin/GroupController', 'index') ?>>
+        <li <?= $this->app->setActive('Admin/GroupController') ?>>
             <?= $this->url->link(t('Groups management'), 'Admin/GroupController', 'index') ?>
         </li>
-        <li <?= $this->app->setActive('Admin/TagController', 'index') ?>>
+        <li <?= $this->app->setActive('Admin/TagController') ?>>
             <?= $this->url->link(t('Tags management'), 'Admin/TagController', 'index') ?>
         </li>
         <li <?= $this->app->setActive('Admin/LinkController') ?>>
             <?= $this->url->link(t('Link settings'), 'Admin/LinkController', 'index') ?>
         </li>
-        <li <?= $this->app->setActive('Admin/PluginController', 'show') ?>>
+        <li <?= $this->app->setActive('Admin/PluginController') ?>>
             <?= $this->url->link(t('Plugins management'), 'Admin/PluginController', 'show') ?>
         </li>
         <?= $this->hook->render('template:config:subside') ?>
