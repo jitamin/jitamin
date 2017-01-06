ea6bd814bba806cc3fbc3dbc9d5b9cf0d6e28127
Guan Shiliang <phecho@163.com>
2017-01-06 16:22:22 +0000

    Apply fixes from StyleCI

--- routes/task.php
+++ routes/task.php
@@ -13,18 +13,18 @@ return [
 
 // Task routes
 'project/{project_id}/task/{task_id}' => 'Task/TaskController@show',
-'t/{task_id}'                        => 'Task/TaskController@show',
+'t/{task_id}'                         => 'Task/TaskController@show',
 'public/task/{task_id}/{token}'       => 'Task/TaskController@readonly',
 
-'task/{project_id}/create'                          => 'Task/TaskController@create',
+'task/{project_id}/create'                            => 'Task/TaskController@create',
 'task/{project_id}/{column_id}/{swimlane_id}/create'  => 'Task/TaskController@create',
-'task/{project_id}/store'                           => 'Task/TaskController@store',
-'project/{project_id}/task/{task_id}/start'          => 'Task/TaskController@start',
-'project/{project_id}/task/{task_id}/edit'           => 'Task/TaskController@edit',
-'project/{project_id}/task/{task_id}/update'         => 'Task/TaskController@update',
-'project/{project_id}/task/{task_id}/remove'         => 'Task/TaskSuppressionController@confirm',
-'project/{project_id}/task/{task_id}/close'          => 'Task/TaskStatusController@close',
-'project/{project_id}/task/{task_id}/screenshot'     => 'Task/TaskPopoverController@screenshot',
+'task/{project_id}/store'                             => 'Task/TaskController@store',
+'project/{project_id}/task/{task_id}/start'           => 'Task/TaskController@start',
+'project/{project_id}/task/{task_id}/edit'            => 'Task/TaskController@edit',
+'project/{project_id}/task/{task_id}/update'          => 'Task/TaskController@update',
+'project/{project_id}/task/{task_id}/remove'          => 'Task/TaskSuppressionController@confirm',
+'project/{project_id}/task/{task_id}/close'           => 'Task/TaskStatusController@close',
+'project/{project_id}/task/{task_id}/screenshot'      => 'Task/TaskPopoverController@screenshot',
 
 'project/{project_id}/task/{task_id}/transitions'    => 'Task/TaskController@transitions',
 'project/{project_id}/task/{task_id}/analytics'      => 'Task/TaskController@analytics',
