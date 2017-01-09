42f0b08e789d7fd21025a8ac78a75efb121374d2
phecho <phecho@163.com>
2017-01-09 09:25:38 +0800

    PicoDb

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -11,6 +11,17 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+$dbUrlParser = new PicoDb\UrlParser();
+if ($dbUrlParser->isEnvironmentVariableDefined()) {
+    $dbSettings = $dbUrlParser->getSettings();
+    define('DB_DRIVER', $dbSettings['driver']);
+    define('DB_USERNAME', $dbSettings['username']);
+    define('DB_PASSWORD', $dbSettings['password']);
+    define('DB_HOSTNAME', $dbSettings['hostname']);
+    define('DB_PORT', $dbSettings['port']);
+    define('DB_NAME', $dbSettings['database']);
+}
+
 if (file_exists(__DIR__.'/cache/config.php')) {
     $configAll = require __DIR__.'/cache/config.php';
     $config = $configAll['config'];
