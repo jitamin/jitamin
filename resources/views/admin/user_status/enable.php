b82bc27562e802f23fee0e8f2d6384a916278194
zhqfdn <luffy@qi-cloud.com>
2017-11-22 16:12:43 +0800

    修改【后台-用户管理-禁用】报错BUG (#232)

--- resources/views/admin/user_status/enable.php
+++ resources/views/admin/user_status/enable.php
@@ -3,13 +3,14 @@
 </div>
 
 <form action="<?= $this->url->href('Admin/UserStatusController', 'enable', ['user_id' => $user['id']]) ?>" method="post" autocomplete="off">
-<div class="confirm">
-    <p class="alert alert-info"><?= t('Do you really want to enable this user: "%s"?', $user['name'] ?: $user['username']) ?></p>
-
-    <div class="form-actions">
-        <button type="submit" class="btn btn-danger"><?= t('Confirm') ?></button>
-        <?= t('or') ?>
-        <?= $this->url->link(t('cancel'), 'Admin/UserController', 'index', [], false, 'close-popover') ?>
-    </div>
-</div>
+	<?= $this->form->csrf() ?>
+	<div class="confirm">
+	    <p class="alert alert-info"><?= t('Do you really want to enable this user: "%s"?', $user['name'] ?: $user['username']) ?></p>
+	
+	    <div class="form-actions">
+	        <button type="submit" class="btn btn-danger"><?= t('Confirm') ?></button>
+	        <?= t('or') ?>
+	        <?= $this->url->link(t('cancel'), 'Admin/UserController', 'index', [], false, 'close-popover') ?>
+	    </div>
+	</div>
 </form>
