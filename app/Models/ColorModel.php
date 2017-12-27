c073e3a4650857f09d19a1c5ddb11840bc42271f
lianguan <guan.shiliang@gmail.com>
2017-12-27 16:50:16 +0800

    Update

--- app/Models/ColorModel.php
+++ app/Models/ColorModel.php
@@ -26,7 +26,7 @@ class ColorModel extends Model
     private $default_colors = [
         'white' => [
             'name'              => 'White',
-            'border-left-color' => '#FFFFFF',
+            'border-left-color' => '#cccccc',
             'border-width'      => '3px',
         ],
         'yellow' => [
