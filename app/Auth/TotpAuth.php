d4361f3d03ed2169a1c71944c55cdd839093c1a0
lianguan <guan.shiliang@gmail.com>
2017-12-28 16:47:28 +0800

    Update

--- app/Auth/TotpAuth.php
+++ app/Auth/TotpAuth.php
@@ -11,7 +11,7 @@
 
 namespace Jitamin\Auth;
 
-use ParagonIE\ConstantTime\Base32;
+use Base32\Base32;
 use Jitamin\Foundation\Base;
 use Jitamin\Foundation\Security\PostAuthenticationProviderInterface;
 use Otp\GoogleAuthenticator;
