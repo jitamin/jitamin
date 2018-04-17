189f55236766abc9e08b21d92915afcc8bcd4ebe
jian玄冰 <jianxuanhuo1@126.com>
2018-04-17 12:53:24 +0800

    修改格式

--- app/Foundation/Csv.php
+++ app/Foundation/Csv.php
@@ -175,8 +175,8 @@ class Csv
 
         if (is_resource($fp)) {
             // wirte BOM header,Solve utf8 chinese grabled problem.
-            fwrite ($fp, chr(0xEF).chr(0xBB).chr( 0xBF ));
-            
+            fwrite ( $fp , chr ( 0xEF ) . chr ( 0xBB ) . chr ( 0xBF ) );
+
             foreach ($rows as $row) {
                 fputcsv($fp, $row, $this->delimiter, $this->enclosure);
             }
